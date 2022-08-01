6bb94bb4c11ded5d45135e2d3d66cce71b418a5d
ammo <ammo@noobuntu.lan>
2022-08-01 10:15:12 +0630

    added helmet and compression middlewares

--- server/express.js
+++ server/express.js
@@ -1,6 +1,8 @@
 import express, { urlencoded } from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import helmet from 'helmet'
+import compression from 'compression'
 
 // import routes
 import invoiceRouter from './routes/invoice.router.js';
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 // routes
 app.use('/api/v1/invoices', invoiceRouter);
